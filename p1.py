10d2ff15f8effc4001528cba668acd173f9b0a77
Ben_Longs_Mother <bdaniels@friendsbalt.org>
2025-02-26 20:34:00 +0000

    Merci Monsieur Hammer

--- p1.py
+++ p1.py
@@ -1,18 +1,15 @@
-
-
-
 def group_anagrams(words):
-    #2. Create lists
+    #1. Create lists
     hash_Value_word_list = {}
-    #3. Create hash values for all of the words
+    #2. Create hash values for all of the words
     for i in words:
         totally_sorted_word = totally_sorted_word.sort
         hash_Value_word_list.append(hash(i))
-    #4. Group words by same hash value
+    #3. Group words by same hash value
         if totally_sorted_word in hash_Value_word_list:
             hash_Value_word_list[totally_sorted_word].append(i)
         else:
             hash_Value_word_list[totally_sorted_word] = i
     # some sort of .sort function
     return list(hash_Value_word_list)
-    #5. Output
+    #4. Output
